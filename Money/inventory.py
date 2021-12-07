e0eb1420d11274368ad22b719632d60fd16622c6
EpicHype <simonas.goda@gmail.com>
2021-12-07 10:57:00 +0200

    TIles and inventory classes

---
+++ Money/inventory.py
@@ -0,0 +1,17 @@
+#import playerNum from char select
+class Player:
+    def __init__(self, playerID, balance, ownedProperty, icon=""):
+        self.playerID = playerID
+        self.icon = icon
+        self.balance = balance
+        self.ownedProperty = ownedProperty
+
+players = []
+
+playerNum = 2# take player num from character selection
+for i in range(playerNum): #gives every player starting stats
+    user = Player(i, 1500, [])
+    # players.append({"playerID": user.playerID, "balance": user.balance, "ownedProperty": user.ownedProperty})
+    players.append(user)
+#print(players[1]["balance"])
+print(players[0].balance)

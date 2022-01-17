1f851279de5ad1b2789b1b46ae5664c161f199b8
VinceKaj <35348750+VinceKaj@users.noreply.github.com>
2022-01-17 14:37:29 +0200

    Created initialize function for property and tile objects

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import modules.cards as cardManager
+import modules.tiles as tilesManager
 import modules.player as player
 
 
@@ -9,9 +10,14 @@ cards = {
   "chance": cardManager.initialize("chance")
 }
 
+tiles = tilesManager.initalizeProperty()
+# print(tiles["1"].property.description)
+
 icons = [] # must be defined in the selection screen, when choosing icons
 players = player.initialize(icons)
 
-print(cards["chest"][2].description)
+# print(cards["chest"][2].description)
+
+""" UI Initialization"""
 
 import uiengine as ui
--- modules/tiles.py
+++ modules/tiles.py
@@ -1,12 +1,14 @@
 class Property:
-    def __init__(self,price,housePrice,color,name,rentCost,houseNumber=0,ownerID=-1):
+    def __init__(self, price=0,housePrice=0,color="none; error?",name="",rent=[0,0,0,0,0,0],houseNumber=0,ownerID=-1, description="", mortgage=0):
         self.price = price
         self.ownerID = ownerID # -1 by default, meaning no owner
         self.color = color
         self.name = name
-        self.rentCost = rentCost
+        self.description = description
+        self.rent = rent
         self.housePrice = housePrice
         self.houseNumber = houseNumber
+        self.mortgage = mortgage
 
 propertyNumber = 28 #total number of all purchasable properties
 
@@ -18,6 +20,22 @@ class Tile:
         self.type = type
         self.property = property
 
-def initialize():
-    # this will be fun
-    pass
+def initalizeProperty():
+
+
+    file = open(f"./assets/sheets/properties.csv", "r")
+    content = file.read()
+    file.close()
+
+    split = content.split('\n') # create array of each line of treausre file
+    split.pop(0) # removes the header line
+
+    tileArr = {}
+    for line in split: # loops through each line
+        lineArr = line.split('#') # array of elements in each card
+        instance = Property(name=lineArr[0], price=lineArr[2], description=lineArr[3], mortgage=lineArr[4], rent=[lineArr[4:8]], color=lineArr[9]) # creates a property object, fills in everything
+        tileInstance = Tile(lineArr[1], "property", instance) # creates a tile instance and puts new property inside of it
+
+        tileArr[lineArr[1]] = tileInstance
+
+    return tileArr
